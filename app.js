1c0db427be564ba9f8d206431d6ae2600b0c27e3
hraj37378 <hraj37378@gmail.com>
2024-12-30 18:37:13 +0530

    trying dummy middlewares

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const app = express();
 
+const { adminAuth, userAuth } = require("./middlewares/auth");
 // app.use("/", (req,res) => {
 //     res.send("Homepage");
 // });
+
+app.use("/admin", adminAuth);
+app.use("/user", userAuth);
 
 app.get("/user", (req, res) => {
     res.send({firstName: "Harsh", lastName: "Raj"});
@@ -16,9 +20,18 @@ app.post("/user", async(req,res) => {
     res.send("Data saved successfully to DB");
 });
 
-app.delete("/user", (req,res) => {
+app.delete("/user", userAuth, (req,res) => {
     res.send("Deleted successfully");
 });
+
+app.get("/admin/getAllData", adminAuth, (req, res) => {
+    res.send("All data sent");
+});
+
+app.post("/admin", (req, res) => {
+    res.send("All data posted on server");
+});
+
 
 app.listen(3000, () => {
     console.log("Server running on PORT:", 3000);
